64ab2e7923f45264433ceedfa30b4032c1987a9c
victorlirafront <pj.victor@wine.com.br>
2023-02-08 10:38:14 -0300

    feat: pageview test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react'
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
 import ProductDetail from './pages/ProductDetail';
 import MainHeader from './components/MainHeader';
+import ReactGA from 'react-ga';
+
+function usePageViews(){
+  let location = useLocation();
+
+  useEffect(() => {
+    console.log('triggered')
+    console.log(window.GA_INITIALIZED)
+    if(!window.GA_INITIALIZED){
+      ReactGA.initialize("G-L49RSBMT76");
+      window.GA_INITIALIZED = true
+    }
+
+    ReactGA.set({page: location.pathname})
+    ReactGA.pageview(location.pathname)
+  }, [location])
+}
 
 function App() {
+  usePageViews()
+
   return (
     <div>
       <MainHeader />
